b55836c602280df713ea206251784cb07525ab06
StandardGiraffe <dannyfekete@gmail.com>
2018-10-09 21:19:35 +0000

    Completed exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,17 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Schmitty", last_name: "McDougal", hourly_rate: 40)
+@store1.employees.create(first_name: "Leeroy", last_name: "Smith", hourly_rate: 30)
+@store1.employees.create(first_name: "Donald", last_name: "Franklinson", hourly_rate: 60)
+@store2.employees.create(first_name: "Vanessa", last_name: "Google", hourly_rate: 25)
+@store2.employees.create(first_name: "Antonio", last_name: "Fekete", hourly_rate: 16)
+@store2.employees.create(first_name: "Cornelius", last_name: "FitzDonald", hourly_rate: 90)
+@store2.employees.create(first_name: "Amanda", last_name: "the Great", hourly_rate: 120)
+@store3.employees.create(first_name: "Sarah", last_name: "Scapelli", hourly_rate: 70)
+@store3.employees.create(first_name: "Billimina", last_name: "Trump", hourly_rate: 20)
+@store3.employees.create(first_name: "Brunhilde", last_name: "Valkyrie", hourly_rate: 20)
+
+
+
