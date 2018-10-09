c659ac24a01f04df155ff588f784ee574aa4bf51
StandardGiraffe <dannyfekete@gmail.com>
2018-10-09 23:04:13 +0000

    Done, geesus.

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,25 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+print "Input a new store name: "
+store_name = gets.chomp
+print "Input revenue: "
+store_revenue = gets.chomp
+print "Menswear? true / false: "
+store_mens = gets.chomp
+# store_mens = ((store_mens == "y") ? true : false)
+
+print "Womenswear? true / false: "
+store_womens = gets.chomp
+# store_womens = ((store_womens == "y") ? true : false)
+
+store = Store.create(
+  name: store_name,
+  annual_revenue: store_revenue,
+  mens_apparel: store_mens,
+  womens_apparel: store_womens
+)
+
+puts store.errors.full_messages
+# puts Store.find_by(name: store_name)
--- lib/employee.rb
+++ lib/employee.rb
@@ -1,5 +1,9 @@
 class Employee < ActiveRecord::Base
 
   belongs_to :store
+  validates :first_name, :last_name, :store_id, presence: true
+  # message: "All fields are required." }
+  validates :hourly_rate, numericality: { greater_than: 40, less_than: 200, message: "Hourly rate must be between $40 and $200"}
+  validates :hourly_rate, numericality: { is_integer: true, message: "Hourly rate must be an integer." }
 
 end
